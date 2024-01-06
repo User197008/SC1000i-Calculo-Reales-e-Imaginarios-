d3e8d8886bac0a32f173c9139122b6092ea264b5
User197008 <155701320+User197008@users.noreply.github.com>
2024-01-06 13:58:38 +0900

    Create math.java

---
+++ math.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+/**
+ *
+ * @author jaime
+ */
+class math {
+
+    static float pow(float parseFloat, float parseFloat0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
