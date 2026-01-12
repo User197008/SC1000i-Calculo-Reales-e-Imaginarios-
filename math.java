cf3f499f53119307b29fc9f1e3e7700f84f3516c
Jaime Jose Goga Nakayoshi <155701320+User197008@users.noreply.github.com>
2026-01-12 10:39:36 +0900

    Update math.java

--- math.java
+++ math.java
@@ -6,8 +6,5 @@ package calculadora;
  */
 class math {
 
-    static float pow(float parseFloat, float parseFloat0) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+   
 }
