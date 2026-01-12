4e6d599388543d64db38fb1746c3d00178a8c3d6
Jaime Jose Goga Nakayoshi <155701320+User197008@users.noreply.github.com>
2026-01-12 10:39:15 +0900

    Update iiiReturn.java

--- iiiReturn.java
+++ iiiReturn.java
@@ -5,15 +5,5 @@ package calculadora;
  * @author jaime
  */
 public class iiiReturn {
-    public static String iii (String cadena) {
-        
-        String cadena1 = cadena.replace("+ i", " 1 i");
-        
-        String cadena2 = cadena1.replace("- i", "- 1 i");
-        
-        String cadena3 = cadena2.replace("( i", "( 1 i");
-        
-        return cadena3;
-        
-    }  
+    
 }
