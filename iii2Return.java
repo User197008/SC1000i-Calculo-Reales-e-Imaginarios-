7bccd713623b08644d6da7cee540e3d332497e4d
Jaime Jose Goga Nakayoshi <155701320+User197008@users.noreply.github.com>
2026-01-12 10:38:52 +0900

    Update iii2Return.java

--- iii2Return.java
+++ iii2Return.java
@@ -7,21 +7,6 @@ package calculadora;
 public class iii2Return {
     public static String iii (String cadena) {
         
-        String cadena1 =  cadena.replace("( i +", "( 1 i +");
-        
-        String cadena2 = cadena1.replace("( i -", "( 1 i -");
-        
-        String cadena3 = cadena2.replace("+ i +", "+ 1 i +");
-        
-        String cadena4 = cadena3.replace("+ i -", "+ 1 i -");
-        
-        String cadena5 = cadena4.replace("- i -", "- 1 i -");
-        
-        String cadena6 = cadena5.replace("- i +", "- 1 i +");
-        
-        String cadena7 = cadena6.replace("+ i )", "+ 1 i )");
-        
-        String cadena8 = cadena7.replace("- i )", "- 1 i )");
         
         
         
